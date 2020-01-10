157c79958b7e50485e9600a639284a3b8742b6c4
iciwiconquer <iciwiconquer@gmail.com>
2020-01-09 22:14:31 -0500

    Addinga  file to master

---
+++ applicationLogic/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+   public static void main(String[] args){
+       System.out.println("Hello World");
+       String someToken = "bdkekaqera;dfkasdfaswieqperoDqerfansdfASdadfadfqwerasdEEESsdqert533";
+       System.out.println(someToken);
+   }
+}
